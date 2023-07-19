c70d8d2a947d80229c2ddf65da14e2ea6d248175
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-19 11:00:04 -0500

    create the initialize and adult method

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,2 +1,21 @@
 class Passenger
+  attr_accessor :name,
+    :age
+
+  def initialize(passenger_details)
+    @name = passenger_details[:name]
+    @age = passenger_details[:age]
+  end
+
+  def adult?
+    @age >= 18
+  end
+
+  def driver?
+    # checks what a driver value is
+  end
+
+  def drive
+    # make a passenger a driver = true
+  end
 end
